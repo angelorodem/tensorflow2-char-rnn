3612d73dd5ea6a9a74d513793b212a36087f517f
Ângelo Rodem <angelorodem@gmail.com>
2019-09-18 20:44:55 -0300

    test didnt work, increased docker parameters

--- train-names.sh
+++ train-names.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --gpus all -it --rm -v $PWD:/tmp:rw -w /tmp train-docker:latest python3 train.py --data_path ./name_data/names.txt \
---save_path ./name_save --n_units 128 --n_layers 2 --n_embedding 128 --epochs 150 --continue True
+--save_path ./name_save --n_units 256 --n_layers 3 --n_embedding 128 --epochs 2000 --continue True
 
 
 
