3382ba4a6afdc6039aad78ebc87bc9025b2db9ff
py <you@example.com>
2019-09-18 16:47:40 -0300

    upd: updated docker scripts that did not contain correct arguments

--- eval-names.sh
+++ eval-names.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-docker run --gpus all -it --rm -v $PWD:/tmp:rw -w /tmp train-docker:latest python3 eval.py --save_path ./name_save --data_path ./name_data/names.txt \
---temperature 0.5 --n_units 256 --n_embedding 128
+docker run --gpus all -it --rm -v $PWD:/tmp:rw -w /tmp train-docker:latest python3 eval.py --save_path ./name_save --temperature 0.5
 
 
 
