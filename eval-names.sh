03c06abf490e14d664dea1da1c5933a42f7b4ad3
Ângelo Rodem <angelorodem@gmail.com>
2019-09-18 22:56:04 -0300

    Added sample data

--- eval-names.sh
+++ eval-names.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --gpus all -it --rm -v $PWD:/tmp:rw -w /tmp train-docker:latest python3 eval.py --save_path ./name_save --temperature 0.5
+docker run --gpus all -it --rm -v $PWD:/tmp:rw -w /tmp train-docker:latest python3 eval.py --save_path ./names_example/sample_checkpoint_data --temperature 0.5
 
 
 
